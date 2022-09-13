af3975c8047adf302ff82f9f59781a739953843d
Vitalii Kulykivskyi <kulvetal@gmail.com>
2022-09-13 10:05:06 +0300

    Make docker compose work. Configure logging

--- Sandbox/Messaging/Kafka/ProducerApi/ProducerApi/Program.cs
+++ Sandbox/Messaging/Kafka/ProducerApi/ProducerApi/Program.cs
@@ -1,8 +1,11 @@
 using Application.Messaging;
 using Infrastructure.Kafka;
 using MediatR;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(builder.Configuration));
 
 // Add services to the container.
 
@@ -11,7 +14,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddKafkaProducer(builder.Configuration)
+builder.Services
+    .AddKafkaProducer(builder.Configuration)
     .AddMediatR(typeof(CreateTopicCommandHandler));
 
 var app = builder.Build();
@@ -20,10 +24,16 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+app.UseSerilogRequestLogging();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGet("/", () => "Hello from ProducerApi");
+
+app.Logger.LogInformation("Producer API started.");
 
 app.Run();
+
